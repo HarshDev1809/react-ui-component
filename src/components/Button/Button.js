97404cafb793e8d791bd36df02afdd303f76ea21
HarshDev1809 <harsh.dev182000@gmail.com>
2024-09-10 02:03:09 +0530

    new commit: Button Component style update.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,49 @@
-import React from "react";
-import ButtonStyle from "./Button.module.css"
+import React, { useEffect } from "react";
+import ButtonStyle from "./Button.module.css";
+import { buttonStyle } from "../../utils/constants/defaultCSS";
+import commonStyle from "../../utils/constants/commonCSS.module.css"
 
-function Button({onClick, placeHolder}){
-    return <button className={ButtonStyle.btn} onClick={onclick ? onClick : ()=>{console.log("no function")}}>{placeHolder? placeHolder : "Click me"}</button>
+function Button({
+  onClick,
+  placeHolder,
+  variant = "default",
+  height = 8,
+  width = 10,
+  fontSize = 1,
+  borderRadius = 7,
+  shadow = false,
+  minWidth = 100,
+  minHeight = 50
+}) {
+
+  const style = {
+    height: `${height}vh`,
+    width: `${width}vw`,
+    fontSize: `${fontSize}rem`,
+    borderRadius: `${borderRadius}px`,
+    minWidth: `${minWidth}px`,
+    minHeight: `${minHeight}px`
+  };
+
+  const finalStyle = {...buttonStyle, ...style};
+
+  const buttonClass = ButtonStyle[variant];
+
+  return (
+    <button
+      className={`${buttonClass} ${shadow? commonStyle.shadow : ""}`}
+      style={finalStyle}
+      onClick={
+        onclick
+          ? onClick
+          : () => {
+              console.log("no function");
+            }
+      }
+    >
+      {placeHolder ? placeHolder : "Click me"}
+    </button>
+  );
 }
 
 export default Button;
---
+++ src/utils/constants/defaultCSS.js
@@ -0,0 +1,9 @@
+const buttonStyle = {
+    cursor : "pointer",
+    border : "none",
+    transition: "background-color 0.3s;"
+}
+
+module.exports = {
+    buttonStyle
+}
